245a1a29fe17bade7cf68a15fb6feb574864635d
David Oliver <david.oliver@nventive.com>
2019-12-20 15:41:40 -0500

    [WASM] Fix bug where clipping could be disabled
    Ensure that setting another property won't remove the 'clippedtoBounds' class, by requiring all calls to 'SetStyle' to pass the RequiresClipping parameter.
    
    This fixes a bug where clipping to bounds could be disabled, which was manifesting when scrolling in a list.

--- doc/ReleaseNotes/_ReleaseNotes.md
+++ doc/ReleaseNotes/_ReleaseNotes.md
@@ -21,6 +21,7 @@
 - #2287 Vertical `ListView` containing a horizontal `ScrollViewer`: horizontal scrolling is difficult, only works when the gesture is perfectly horizontal
 - #2130 Grid - fix invalid measure when total star size is 0
 - [iOS] Fix invalid image measure on constrained images with `Margin`
+- [WASM] Fix bug where changing a property could remove the required clipping on a view
 
 ## Release 2.0
 
--- src/Uno.UI/UI/Xaml/UIElement.wasm.cs
+++ src/Uno.UI/UI/Xaml/UIElement.wasm.cs
@@ -135,12 +135,12 @@ namespace Windows.UI.Xaml
 
 		protected internal void SetStyle(string name, string value)
 		{
-			Uno.UI.Xaml.WindowManagerInterop.SetStyles(HtmlId, new[] { (name, value) });
+			Uno.UI.Xaml.WindowManagerInterop.SetStyles(HtmlId, new[] { (name, value) }, setAsArranged: false, clipToBounds: RequiresClipping);
 		}
 
 		protected internal void SetStyle(string name, double value)
 		{
-			Uno.UI.Xaml.WindowManagerInterop.SetStyleDouble(HtmlId, name, value);
+			Uno.UI.Xaml.WindowManagerInterop.SetStyleDouble(HtmlId, name, value, requiresClipping: RequiresClipping);
 		}
 
 		protected internal void SetStyle(params (string name, string value)[] styles)
@@ -150,7 +150,7 @@ namespace Windows.UI.Xaml
 				return; // nothing to do
 			}
 
-			Uno.UI.Xaml.WindowManagerInterop.SetStyles(HtmlId, styles);
+			Uno.UI.Xaml.WindowManagerInterop.SetStyles(HtmlId, styles, setAsArranged: false, clipToBounds: RequiresClipping);
 		}
 
 		/// <summary>
@@ -188,7 +188,7 @@ namespace Windows.UI.Xaml
 
 		protected internal void SetNativeTransform(Matrix3x2 matrix)
 		{
-			Uno.UI.Xaml.WindowManagerInterop.SetElementTransform(HtmlId, matrix);
+			Uno.UI.Xaml.WindowManagerInterop.SetElementTransform(HtmlId, matrix, requiresClipping: RequiresClipping);
 		}
 
 		protected internal void ResetStyle(params string[] names)
